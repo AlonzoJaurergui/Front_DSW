5e01260906cad44608e28fbab517e3bf0b8f3115
adrym1 <i202110771@cibertec.edu.pe>
2023-11-21 21:12:28 -0500

    Se agregó listado de cliente

---
+++ Controllers/ClienteController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PizzaNicola_AspNetCore.Models.Repositorios;
+using PizzaNicola_AspNetCore.Models.Entidades;
+using Newtonsoft.Json;
+
+namespace PizzaNicola_AspNetCore.Controllers
+{
+    public class ClienteController : Controller
+    {
+        /*private readonly ClienteRepositorio _clienteR;
+
+        public ClienteController(ClienteRepositorio clienteRepositorio)
+        {
+            _clienteR = clienteRepositorio;
+        }*/
+
+        public async Task<IActionResult> Index()
+        {
+            List<Cliente> clientes = new List<Cliente>();
+            try
+            {
+                using (var httpclient = new HttpClient())
+                {
+                    httpclient.BaseAddress = new Uri("http://localhost:8080");
+                    HttpResponseMessage response = await httpclient.GetAsync("api/nicola/clientes/");
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string apiResponse = await response.Content.ReadAsStringAsync();
+                        clientes = JsonConvert.DeserializeObject<List<Cliente>>(apiResponse)
+                        .Select(item => new Cliente()
+                        {
+                            id = item.id,
+                            dni = item.dni,
+                            nombre = item.nombre,
+                            apellidos = item.apellidos,
+                            edad = item.edad,
+                            telefono = item.telefono,
+                            direccion = item.direccion
+                        }).ToList();
+                    }
+                    else
+                    {
+                        return View("ERROR! T-T");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return View(clientes);
+        }
+    }
+}
---
+++ Models/Entidades/Cliente.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System.ComponentModel.DataAnnotations;
+
+namespace PizzaNicola_AspNetCore.Models.Entidades
+{
+    public class Cliente
+    {
+        [Display(Name = "ID")]
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string id { get; set; }
+        [Display(Name = "DNI")]
+        public string? dni { get; set; }
+        [Display(Name = "Nombre")]
+        public string? nombre { get; set; }
+        [Display(Name = "Apellidos")]
+        public string? apellidos { get; set; }
+        [Display(Name = "Edad")]
+        public int edad {  get; set; }
+        [Display(Name = "Teléfono")]
+        public string? telefono { get; set; }
+        [Display(Name = "Dirección")]
+        public string? direccion { get; set; }
+
+    }
+}
